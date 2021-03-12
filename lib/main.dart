2bc7898d37b3d35df239384f321d0a9b3f4a2742
Dhanraj-MobileAppDev <52417093+DhanrajNilkanth@users.noreply.github.com>
2021-03-12 15:04:44 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
-import 'package:flutter/material.dart';import 'package:flutter_login_ui/login_screen.dart';void main() {  runApp(MyApp());}class MyApp extends StatelessWidget {  @override  Widget build(BuildContext context) {    return MaterialApp(        debugShowCheckedModeBanner: false,        home: LoginScreen()      );  }}
+import 'package:flutter/material.dart';
+import 'package:flutter_login_ui/login_screen.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen()
+      );
+  }
+}
+
